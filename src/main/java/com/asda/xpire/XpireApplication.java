14712ca4af3635367a90911722bfcfe0b444fdcc
Luke Towell <luke.towell@hotmail.com>
2020-07-28 14:13:42 +0100

    Added in the spring boot
    - Added in the spring boot initialiser
    - Added in the spring boot git-ignore

---
+++ src/main/java/com/asda/xpire/XpireApplication.java
@@ -0,0 +1,13 @@
+package com.asda.xpire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asda/xpire/XpireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asda.xpire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
