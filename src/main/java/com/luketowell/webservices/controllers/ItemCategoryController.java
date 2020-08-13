7f0ce38f9479552068d0494e5e73d9ce20aa32ea
Luke Towell <luke.towell@hotmail.com>
2020-08-13 20:05:43 +0100

    Updated category controller
    - added endpoints for the get category by id, add new category and delete category by id.

--- src/main/java/com/luketowell/webservices/controllers/ItemCategoryController.java
+++ src/main/java/com/luketowell/webservices/controllers/ItemCategoryController.java
@@ -1,13 +1,15 @@
 package com.luketowell.webservices.controllers;
 
+import com.luketowell.webservices.error.CategoryNotFoundException;
 import com.luketowell.webservices.models.ItemCategory;
 import com.luketowell.webservices.repositories.ItemCategoryRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("itemCategory")
+@RequestMapping("itemcategory")
 public class ItemCategoryController {
 
     private final ItemCategoryRepository itemCategoryRepository;
@@ -19,5 +21,23 @@ public class ItemCategoryController {
     @GetMapping("/all")
     public Iterable<ItemCategory> getItemCategories() {
         return itemCategoryRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ItemCategory getItemCategory(@PathVariable Long id) {
+        return itemCategoryRepository.findById(id)
+                .orElseThrow(() -> new CategoryNotFoundException(id));
+    }
+
+    @PostMapping("/new")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ItemCategory newItemCategory(@RequestBody ItemCategory itemCategory){
+        return itemCategoryRepository.save(itemCategory);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteCategory ( @PathVariable Long id) {
+        itemCategoryRepository.deleteById(id);
     }
 }
