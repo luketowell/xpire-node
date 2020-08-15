453e12369a5c2d44e9f7f4d265aabe78b9849846
Luke Towell <luke.towell@hotmail.com>
2020-08-15 17:01:12 +0100

    Added endpoint for get store item action by id
    - added endpoint for get store item action by id
    - added error handling for unable to find SIA.
    - updated custom global exception handler

--- src/main/java/com/luketowell/webservices/controllers/StoreItemActionController.java
+++ src/main/java/com/luketowell/webservices/controllers/StoreItemActionController.java
@@ -1,12 +1,10 @@
 package com.luketowell.webservices.controllers;
 
+import com.luketowell.webservices.error.StoreItemActionNotFoundException;
 import com.luketowell.webservices.models.StoreItemAction;
 import com.luketowell.webservices.repositories.StoreItemActionRepository;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("actions")
@@ -21,5 +19,11 @@ public class StoreItemActionController {
     @GetMapping("/all")
     public Iterable<StoreItemAction>getAllActions(){
         return storeItemActionRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public StoreItemAction findStoreItemActionById(@PathVariable Long id){
+        return storeItemActionRepository.findById(id)
+                .orElseThrow(() -> new StoreItemActionNotFoundException(id));
     }
 }
