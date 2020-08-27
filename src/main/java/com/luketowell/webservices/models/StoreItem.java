2c0b54179f0a35800ab3949c99a1b8a19c936afc
Luke Towell <luke.towell@hotmail.com>
2020-08-27 11:40:56 +0100

    updated the store item
    - updated the store item model to represent changes in the db

--- src/main/java/com/luketowell/webservices/models/StoreItem.java
+++ src/main/java/com/luketowell/webservices/models/StoreItem.java
@@ -3,6 +3,7 @@ package com.luketowell.webservices.models;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.Date;
 import java.util.Set;
 
 @Data
@@ -21,6 +22,15 @@ public class StoreItem {
     @Column(name="store_id")
     private String storeId;
 
+    @Column(name="expiry_date")
+    private Date expiry_date;
+
+    @Column(name="created_date")
+    private Date created_date;
+
+    @Column(name="updated_date")
+    private Date updated_date;
+
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name="item_upc")
     private Item item;
@@ -31,15 +41,21 @@ public class StoreItem {
     public StoreItem() {
     }
 
-    public StoreItem(Long id, Long item_upc, String storeId) {
+    public StoreItem(Long id, Long item_upc, String storeId, Date expiry_date, Date created_date, Date updated_date) {
         this.id = id;
         this.item_upc = item_upc;
         this.storeId = storeId;
+        this.expiry_date = expiry_date;
+        this.created_date = created_date;
+        this.updated_date = updated_date;
     }
 
-    public StoreItem(Long id, Item item, String store_id) {
+    public StoreItem(Long id, String storeId, Date expiry_date, Date created_date, Date updated_date, Item item) {
         this.id = id;
+        this.storeId = storeId;
+        this.expiry_date = expiry_date;
+        this.created_date = created_date;
+        this.updated_date = updated_date;
         this.item = item;
-        this.storeId = store_id;
     }
 }
